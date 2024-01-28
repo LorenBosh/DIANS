8a1a63a25b9597441bd93704cb9c264ac83f33ec
Nikip31 <nikip006@gmail.com>
2024-01-28 15:49:26 +0100

    Final revision and connection with the microservice for registering

---
+++ Domasna3/user-login-microservice/src/main/java/com/example/userloginmicroservice/web/controller/RegistrationController.java
@@ -0,0 +1,43 @@
+package com.example.userloginmicroservice.web.controller;
+
+import com.example.userloginmicroservice.bootstrap.DataHolder;
+import com.example.userloginmicroservice.model.User;
+import com.example.userloginmicroservice.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/auth")
+public class RegistrationController {
+
+    private final AuthService authService;
+
+    @Autowired
+    public RegistrationController(AuthService authService) {
+        this.authService = authService;
+    }
+    @CrossOrigin(origins = "http://localhost:8080")
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestParam String username,
+                                               @RequestParam String password,
+                                               @RequestParam String repeatedPassword,
+                                               @RequestParam String name,
+                                               @RequestParam String surname) {
+        try {
+            User registeredUser = authService.register(username,password,repeatedPassword,name,surname);
+            if(!Objects.equals(password, repeatedPassword)) {
+               return ResponseEntity.ok().body("Registration failed: password doesnt match ");
+            }else {
+                DataHolder.users.add(registeredUser);
+                return ResponseEntity.ok().body("ok");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.ok().body("Registration failed");
+        }
+    }
+}
+
